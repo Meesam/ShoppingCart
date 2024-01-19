e16bb13c32fd9952879faa01cbfd13b3d15e9dc2
Meesam <meesam.engineer@gmail.com>
2024-01-19 16:17:39 +0530

    Add project files.

---
+++ ShoppingCartWeb/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode:'class',
+    content: ["./Views/**/*.{cshtml,razor}", "./wwwroot/js/**/*.js"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
---
+++ ShoppingCartWeb/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+} else {
+    document.documentElement.classList.remove('dark')
+}
+
+// Whenever the user explicitly chooses light mode
+localStorage.theme = 'light'
+
+// Whenever the user explicitly chooses dark mode
+localStorage.theme = 'dark'
