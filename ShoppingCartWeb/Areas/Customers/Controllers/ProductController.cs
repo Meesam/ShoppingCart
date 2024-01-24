37d0150e5f44a235c7e77c99fb4a8bf73b1d1e1a
Meesam <meesam.engineer@gmail.com>
2024-01-24 14:12:05 +0530

    Customer Product list page UI complete

--- ShoppingCartWeb/Areas/Customers/Controllers/ProductController.cs
+++ ShoppingCartWeb/Areas/Customers/Controllers/ProductController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ShoppingCartWeb.Areas.Customers.Controllers
 {
     [Area("Customers")]
+    [Authorize]
     public class ProductController : Controller
     {
         public IActionResult ProductList()
